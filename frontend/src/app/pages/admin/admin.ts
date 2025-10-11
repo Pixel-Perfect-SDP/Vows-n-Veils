117414c01b118bf26514a9438f36fb6989e91d82
kbWits <2538031@students.wits.ac.za>
2025-10-11 13:07:31 +0200

    Admin notification
    Added a notification if the venue or vendor is rejected

--- frontend/src/app/pages/admin/admin.ts
+++ frontend/src/app/pages/admin/admin.ts
@@ -1,9 +1,10 @@
 import { Component,inject,OnInit } from "@angular/core";
 import { Firestore,collection,query,where,getDocs } from "@angular/fire/firestore";
 import { CommonModule } from "@angular/common";
-import { updateDoc,doc } from "firebase/firestore";
+import { updateDoc,doc, addDoc } from "firebase/firestore";
 import { getAuth,signOut } from "firebase/auth";
 import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 @Component({
 
@@ -11,7 +12,7 @@ import { Router } from "@angular/router";
     templateUrl: './admin.html',
     styleUrls: ['./admin.css'],
     standalone:true,
-    imports: [CommonModule]
+    imports: [CommonModule,FormsModule]
 })
 
 export class Admin implements OnInit
@@ -92,12 +93,30 @@ export class Admin implements OnInit
         }
         }
 
-    async changeStatus(collectionName:string,docId:string,newStatus:string)
+    async changeStatus(collectionName:string,docId:string,newStatus:string,reason?:string)
     {
         try
         {
             const docRef = doc(this.db, collectionName, docId);
+            const compID=collectionName==='Vendors' ? this.vendors.find(vendor=>vendor.id===docId)?.companyID : this.venues.find(venue=>venue.id===docId)?.companyID;
             await updateDoc(docRef, { status: newStatus });
+
+            if (newStatus=='rejected')
+            {
+                const reasonCollection = collection(this.db, 'Notifications');
+                const name = collectionName === 'Vendors' ? this.vendors.find(vendor => vendor.id === docId)?.name : this.venues.find(venue => venue.id === docId)?.name;
+                await addDoc(reasonCollection,
+                {
+                    date: new Date().toISOString(),
+                    from:"Admin",
+                    to: compID,
+                    message: `Your ${collectionName.slice(0,-1)} application for ${name} has been rejected becuase ${reason||'no reason provided'}.`,
+                    read: false
+
+                });
+
+            }
+
             if (collectionName === 'Vendors') {
                 this.vendors = this.vendors.filter(vendor => vendor.id !== docId);
             } else if (collectionName === 'Venues') {
