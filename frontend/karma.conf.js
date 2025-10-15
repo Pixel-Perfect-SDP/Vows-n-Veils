fc4079b4d127032ec5a18e292e01ee0e3fd5e9b0
siphoHub <2430285@students.wits.ac.za>
2025-10-15 18:07:26 +0200

    codecov display bug fixes: yml&karma change

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -11,28 +8,28 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
     ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+    client: { jasmine: {} },
+    jasmineHtmlReporter: { suppressAll: true },
+
+    // ✅ write coverage to "frontend/coverage/lcov.info"
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
+      dir: require('path').join(__dirname, 'coverage'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'lcovonly' }, 
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }]
     },
-    reporters: ['progress', 'kjhtml','coverage'],
-    browsers: ['Chrome'],
-    restartOnFileChange: true
+
+    reporters: ['progress', 'kjhtml', 'coverage'],
+
+    // ✅ Use a CI-friendly, sandboxless Chrome
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
+
+    singleRun: true,          // CI mode
+    restartOnFileChange: false
   });
 };
